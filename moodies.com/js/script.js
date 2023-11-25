2ec534d306a8cba76fa0e6f90622b42cca773f4d
Σero Technologies Inc <147784136+Eero-tech-Inc@users.noreply.github.com>
2023-11-24 19:21:33 -0800

    Add files via upload

--- moodies.com/js/script.js
+++ moodies.com/js/script.js
@@ -12,31 +12,3 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
     cart.classList.remove("active");
 };
-
-/*const serviceItems = document.querySelector(".header-info");
-const popup = document.querySelector(".header-popup-body")
-const popupCloseBtn = popup.querySelector(".close-btn");
-
-serviceItems.addEventListener("click", function(event){
-    if(event.target.tagName.toLowerCase() == "read-btn"){
-        const item = event.target.parentElement;
-        const h3 = item.querySelector("h3").innerHTML;
-        const readMoreCont = item.querySelector(".header-popup").innerHTML;
-        popup.querySelector("h3").innerHTML = h3;
-        popup.querySelector(".header-popup-body").innerHTML = readMoreCont;
-
-        popupBox();
-  } 
-})
-
-popupCloseBtn.addEventListener("click", popupBox);
-
-popup.addEventListener("click", function(event){
-    if (event.target == popup){
-        popupBox();
-    }
-})  
-
-function popupBox(){
-    popup.classList.toggle("open");
-}*/
